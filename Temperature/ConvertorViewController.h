3990266af57f514bf8857151cebe9c563f96450d
Jia Yang <jyang@00633a-rochan.corp.zynga.com>
2014-01-21 16:36:31 -0800

    first

---
+++ Temperature/ConvertorViewController.h
@@ -0,0 +1,15 @@
+//
+//  ConvertorViewController.h
+//  Temperature
+//
+//  Created by Jia Yang on 1/18/14.
+//  Copyright (c) 2014 zynga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConvertorViewController : UIViewController
+
+
+
+@end
---
+++ Temperature/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  Temperature
+//
+//  Created by Jia Yang on 1/16/14.
+//  Copyright (c) 2014 zynga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
